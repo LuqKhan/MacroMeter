687052bf71b653e03565d947f8138a89620c0986
LuqmaanKhan <luq.j.khan@gmail.com>
2020-08-29 14:06:22 -0500

    Add launch screen and labels/text field for user info

---
+++ MacroMeter/MacroMeter/ViewControllers/GetStartedVC/GetStartedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GetStartedViewController.swift
+//  MacroMeter
+//
+//  Created by Luqmaan Khan on 8/29/20.
+//  Copyright © 2020 Luqmaan Khan. All rights reserved.
+//
+
+import UIKit
+
+class GetStartedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
